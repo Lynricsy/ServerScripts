a152cc884a6d0930e413a7a041e445d9d3d743ce
Lynricsy <im@ling.plus>
2025-12-13 16:52:28 +0800

    🐛 fix: 修复 virt-customize 中 HOME 环境变量未设置的问题
    - 在 Zim 安装命令前显式设置 HOME=/root
    - 在 zimfw install 命令前也设置 HOME=/root
    - 修复因 HOME 为空导致 Zim 安装到错误目录的问题

--- imagepacker-opensuse.sh
+++ imagepacker-opensuse.sh
@@ -91,9 +91,9 @@ virt-customize -a openSUSE-Tumbleweed-Minimal-VM.x86_64-Cloud.qcow2 \
   ]
 }
 EOF" \
-  --run-command "curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh" \
+  --run-command "export HOME=/root && curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh" \
   --run-command "grep -qx 'zmodule romkatv/powerlevel10k --use degit' /root/.zimrc || echo 'zmodule romkatv/powerlevel10k --use degit' >> /root/.zimrc" \
-  --run-command "/root/.zim/bin/zimfw install" \
+  --run-command "export HOME=/root && /root/.zim/bin/zimfw install" \
   --run-command "touch /root/.zshrc" \
   --run-command "grep -qx 'cat /etc/motd' /root/.zshrc || sed -i '1i cat /etc/motd' /root/.zshrc" \
   --run-command "grep -qx 'fastfetch' /root/.zshrc || sed -i '/^cat \\/etc\\/motd$/a fastfetch' /root/.zshrc" \
